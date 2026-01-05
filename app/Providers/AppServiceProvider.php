3d2f3f2180a1b8837500faca0dca177be79dc1fb
kolli26 <kollarandras2006@gmail.com>
2026-01-05 08:40:20 +0100

    Configure API URL and HTTP macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Http::macro('api', function () {
+            return Http::withHeaders([
+                'Accept' => 'application/json',
+            ])->baseUrl(config('services.api.base_uri'));
+        });
     }
 }
